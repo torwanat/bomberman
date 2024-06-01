3c2d777351e96acd9a4ef04dab2c85b357ff09f3
torwanat <74968490+torwanat@users.noreply.github.com>
2024-06-01 17:03:59 +0200

    Add server-side board generation

--- scripts/connect.ts
+++ scripts/connect.ts
@@ -1,15 +1,9 @@
 export default class Connect {
-    private uri: string = "ws://localhost:46089/bomberman/server.php";
     private websocket: WebSocket;
 
-    constructor() {
-        this.websocket = new WebSocket(this.uri);
+    constructor(uri: string) {
+        this.websocket = new WebSocket(uri);
         this.init();
-    }
-
-    public send() {
-        const message: string = "Hello";
-        this.websocket.send(JSON.stringify(message));
     }
 
     private init() {
@@ -17,20 +11,13 @@ export default class Connect {
             console.log("open");
         }
 
-        this.websocket.onmessage = (ev: MessageEvent) => {
-            if (ev.data != "")
-                try {
-                    var msg = JSON.parse(ev.data); //PHP sends Json data
-                    console.log(msg);
-                } catch (error) {
-                    console.error(error);
-                    console.log(ev.data);
-                }
-        }
-
         this.websocket.onerror = (ev) => {
             console.log(ev);
         }
+    }
+
+    public getWebSocket() {
+        return this.websocket;
     }
 
 }
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,6 +1,7 @@
 import Canvas from "./canvas";
 import data from "../res/data.json";
 import Animate from "./animate";
+import Connect from "./connect";
 
 const anim = () => {
     for (let i = 0; i < imgsArray.length; i++) {
@@ -9,50 +10,8 @@ const anim = () => {
     setTimeout(window.requestAnimationFrame, 1000 / 60, anim) // ~60 klatek/s
 }
 
-const prepareStaticBoard = () => {
-    const board: Array<Array<string>> = [];
-
-    for (let i: number = 0; i < 33; i++) {
-        board.push([]);
-        for (let j: number = 0; j < 13; j++) {
-            board[i][j] = "";
-        }
-    }
-
-
-
-    for (let i: number = 0; i < 33; i++) {
-        if (i == 0 || i == 32) {
-            for (let j: number = 0; j < 13; j++) {
-                board[i][j] = "W";
-            }
-        } else if (i % 2 == 0) {
-            for (let j: number = 0; j < 13; j += 2) {
-                board[i][j] = "W";
-            }
-        } else {
-            board[i][0] = "W";
-            board[i][12] = "W";
-        }
-    }
-
-    let counter: number = 40;
-    while (counter > 0) {
-        const x = Math.floor(Math.random() * 32);
-        const y = Math.floor(Math.random() * 12);
-
-        if (board[x][y] == "") {
-            board[x][y] = "B";
-            counter--;
-        }
-    }
-
-    return board;
-}
-
-const staticBoard: Array<Array<string>> = prepareStaticBoard();
-console.log(staticBoard);
-
+const connection: Connect = new Connect("ws://torvan-bomberman.ct8.pl:1984/sockets/server.php");
+const socket: WebSocket = connection.getWebSocket();
 
 const w = new Canvas();
 w.drawWorld();
@@ -62,19 +21,27 @@ const img = new Image();
 img.src = "./res/spritesheet.png";
 
 img.onload = function () {
-    for (let i = 0; i < staticBoard.length; i++) {
-        for (let j = 0; j < staticBoard[i].length; j++) {
-            switch (staticBoard[i][j]) {
-                case "W":
-                    imgsArray.push(new Animate(img, data.wall, data.dimensions, true, { "x": i * 16, "y": j * 16 }, w));
-                    break;
-                case "B":
-                    imgsArray.push(new Animate(img, data.bricks.solid, data.dimensions, true, { "x": i * 16, "y": j * 16 }, w));
-                    break;
-                default:
-                    break;
+    socket.onmessage = (ev: MessageEvent) => {
+        if (ev.data != "") {
+            const board: Array<Array<string>> = JSON.parse(ev.data);
+            console.log(board);
+            for (let i = 0; i < board.length; i++) {
+                for (let j = 0; j < board[i].length; j++) {
+                    switch (board[i][j]) {
+                        case "W":
+                            imgsArray.push(new Animate(img, data.wall, data.dimensions, true, { "x": i * 16, "y": j * 16 }, w));
+                            break;
+                        case "B":
+                            imgsArray.push(new Animate(img, data.bricks.solid, data.dimensions, true, { "x": i * 16, "y": j * 16 }, w));
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
+            anim();
         }
     }
-    anim();
+
+
 }
