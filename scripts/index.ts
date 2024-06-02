a114e3634fbda10990038c06950d6d844726a463
torwanat <nawrotm37@gmail.com>
2024-06-02 10:03:24 +0200

    Refactor animations generation

--- scripts/index.ts
+++ scripts/index.ts
@@ -16,14 +16,46 @@ interface IDataFromServer {
     balloons: Array<Balloon>
 }
 
+const drawBoard = (board: Array<Array<string>>) => {
+    const boardAnimations: Array<Animate> = [];
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            switch (board[i][j]) {
+                case "W":
+                    boardAnimations.push(new Animate(img, data.wall, data.dimensions, true, { "x": i * 16, "y": j * 16 }, w));
+                    break;
+                case "B":
+                    boardAnimations.push(new Animate(img, data.bricks.solid, data.dimensions, true, { "x": i * 16, "y": j * 16 }, w));
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    return boardAnimations;
+}
+
+const drawBalloons = (balloons: Array<Balloon>) => {
+    const balloonsAnimations: Array<Animate> = [];
+    for (let i: number = 0; i < balloons.length; i++) {
+        const balloon: Balloon = balloons[i];
+        if (balloon.alive) {
+            balloonsAnimations.push(new Animate(img, balloon.direction > 1 ? data.balloon.left : data.balloon.right, data.dimensions, true, { "x": balloon.x, "y": balloon.y }, w));
+        } else {
+            balloonsAnimations.push(new Animate(img, data.balloon.death, data.dimensions, false, { "x": balloon.x, "y": balloon.y }, w));
+        }
+    }
+    return balloonsAnimations;
+}
+
 const connection: Connect = new Connect("ws://torvan-bomberman.ct8.pl:1984/sockets/server.php");
 const socket: WebSocket = connection.getWebSocket();
 
 const w = new Canvas();
 w.drawWorld();
 
-
-const imgsArray: Animate[] = [];
+let boardAnimations: Array<Animate> = [];
+let balloonsAnimations: Array<Animate> = [];
 const img = new Image();
 img.src = "./res/spritesheet.png";
 Animate.incrementTick();
@@ -31,34 +63,15 @@ Animate.incrementTick();
 img.onload = function () {
     socket.onmessage = (ev: MessageEvent) => {
         if (ev.data != "") {
-            Animate.clearArrayAnimations(imgsArray);
-            imgsArray.length = 0;
+            Animate.clearArrayAnimations(boardAnimations);
+            Animate.clearArrayAnimations(balloonsAnimations)
+            boardAnimations.length = 0;
+            balloonsAnimations.length = 0;
             const dataFromServer: IDataFromServer = JSON.parse(ev.data);
-            const staticPropsBoard: Array<Array<string>> = dataFromServer.board;
-            const balloons: Array<Balloon> = dataFromServer.balloons;
-            for (let i = 0; i < staticPropsBoard.length; i++) {
-                for (let j = 0; j < staticPropsBoard[i].length; j++) {
-                    switch (staticPropsBoard[i][j]) {
-                        case "W":
-                            imgsArray.push(new Animate(img, data.wall, data.dimensions, true, { "x": i * 16, "y": j * 16 }, w));
-                            break;
-                        case "B":
-                            imgsArray.push(new Animate(img, data.bricks.solid, data.dimensions, true, { "x": i * 16, "y": j * 16 }, w));
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-            for (let i: number = 0; i < balloons.length; i++) {
-                const balloon: Balloon = balloons[i];
-                if (balloon.alive) {
-                    imgsArray.push(new Animate(img, balloon.direction > 1 ? data.balloon.left : data.balloon.right, data.dimensions, true, { "x": balloon.x, "y": balloon.y }, w));
-                } else {
-                    imgsArray.push(new Animate(img, data.balloon.death, data.dimensions, false, { "x": balloon.x, "y": balloon.y }, w));
-                }
-            }
-            Animate.animateArray(imgsArray);
+            boardAnimations = drawBoard(dataFromServer.board);
+            balloonsAnimations = drawBalloons(dataFromServer.balloons);
+            Animate.animateArray(boardAnimations);
+            Animate.animateArray(balloonsAnimations);
         }
     }
 
